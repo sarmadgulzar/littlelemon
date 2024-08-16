4747330a0f0abfe95d62bc4f07a364a2aa02631f
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2024-08-16 22:59:09 +0500

    Added menu and booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register(r"booking", views.BookingViewSet)
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("menu/", views.MenuListCreateAPIView.as_view(), name="menu-list-create"),
+    path("menu/<int:pk>/", views.MenuDetailAPIView.as_view(), name="menu-detail"),
+    path("booking/", include(router.urls)),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 
 def index(request):
     return render(request, "restaurant/index.html", {})
+
+
+class MenuListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class MenuDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
